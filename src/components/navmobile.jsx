1e7a43fdc4dfb36d77c36d13adb9ab2b4de963bf
Divyanssss <divyanship19@gmail.com>
2025-09-29 19:32:02 +0530

    changes

--- src/components/navmobile.jsx
+++ src/components/navmobile.jsx
@@ -18,11 +18,11 @@ export default function MobileStickyNavbar() {
           <button className="relative inline-flex h-12 overflow-hidden rounded-xl p-[1px] focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#FF315E_0%,#E861FF_50%,#5e1fff_75%,#FF315E_90%)]" />
             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-xl bg-gray-950 px-8 py-0 text-sm font-medium text-gray-50 backdrop-blur-3xl">
-              <div className="py-2">Register</div>
+              <div className="pt-1 pb-2 px-2">Register</div>
               <img
                 src={arrowWhite}
                 alt="arrow"
-                style={{ height: 16, marginLeft: 6 }}
+                style={{ height: 16, marginLeft: 4 }}
               />
             </span>
           </button>
